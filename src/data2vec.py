d7256ab94d1c3c8dd25304a4cd58c268e5d1426a
Aayush Gogia <aayushar@yahoo.com>
2018-04-30 23:18:49 +0530

    Added data2vec file

--- src/data2vec.py
+++ src/data2vec.py
@@ -3,4 +3,36 @@ import datetime
 import random
 
 #Third Party libraries
-import numpy
+import numpy as np
+
+
+class vectorizeData:
+    '''
+    This class transforms the text data into a vector that
+    would be used as an input to our scRNN model.
+    Structure of the vector: [Beg,Int,End]
+    '''
+    def __init(self, TrainPath, TestPath):
+        '''
+        inp:
+        TrainPath -> Path of training data
+        TestPath -> Path of testing data
+
+        '''
+        self.TrainPath = TrainPath
+        self.TestPath = TestPath
+        self.alph = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz.,:;'*!?`$%&(){}[]-/\@_#"
+
+
+    def loadData(self, path):
+        '''
+        Load the data from the path
+        '''
+        words = open(path).read().replace('\n', '<eos>')
+        wordsDataset = np.ndarray((len(words),), dtype=np.int32)
+
+        for i,word in enumerate(words):
+            #Generate one hot encoding for every word
+            if word not in vocab:
+                vocab[word] = len(vocab)
+                idx2vocab[vocab[words]] = word
